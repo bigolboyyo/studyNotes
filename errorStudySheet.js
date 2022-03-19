49d8fe1f1c5115cb58fc139a1ebb72ab762364ba
Wesley <wesley.grant94@gmail.com>
2022-03-19 10:28:44 -0400

    another study sheet

---
+++ errorStudySheet.js
@@ -0,0 +1,29 @@
+// studySheet for understanding Errors and Stack Traces
+
+TODO:
+Uncaught ReferenceError: ____ is not defined
+
+// a common and simple error. We tried to reference a var or funct 
+// that doesn't exist in the current scope
+// can also arise if you forget quotation around a string
+
+TODO:
+Uncaught TypeError: ____ is not a function
+
+// usually indicates an attempt at invoking something that isn't a function
+
+/* ERROR VARIANT */  Uncaught TypeError: undefined is not a function
+// we attempt to invoke undefined. We attempt to store a function in a var, but var is undefined
+
+// const oh = ["Insert", "Spread", "Joke"]
+
+Uncaught SyntaxError: missing ) after argument list
+// you tried to function, but you never closed your parenthesis :*(( ...oh wait I mean ))
+
+Uncaught TypeError: Assignment to constant variable
+// you attempted to assign a new value to a var declared with const
+// this can also simply just be a very small typo
+
+
+
+
