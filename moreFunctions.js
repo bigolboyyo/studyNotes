3ad25be10a58b84df059f8284f447d0897006323
Wesley <wesley.grant94@gmail.com>
2022-03-22 11:31:21 -0400

    Update before next functions lab cloning

---
+++ moreFunctions.js
@@ -0,0 +1,102 @@
+// continued studies of functions in JavaScript functions
+
+// sayHi() // you can run and invoke your function even before the function is declared!!
+
+function sayHi(name) {
+    console.log('Hello ' + name)
+}
+// sayHi('Ash') // You must invoke your function!!
+
+// hiRose() /* notice how calling the hiRose function doesn't work here */
+// this is because of HOISTING
+
+const hiRose = function () {
+    console.log('Hi Rose')
+}
+/* this is an anonymous function */
+
+// hiRose() /* this one calls because we declared an anonymous function and defined it before this
+
+const doMath = function (number, num2) {
+    /*console.log(number * num2)*/
+    return (
+        number * num2
+    ) /* will complete this operation before returning the value */
+}
+console.log(doMath(5, 5))
+
+//NOTE: Using return is needed if we want the value of our function to be "interactable"
+
+let fiveByFive = doMath(5, 5)
+
+console.log(fiveByFive)
+
+// PRACTICE
+
+const blankArray = []
+const practiceArray = ['This', 'Is', 'The', 'String', 'Of', 'An', 'Array']
+const nestedObject = {
+    nest1: [
+        {
+            subNest1: 'subNest1 data',
+            subNest2: 'subNest2 data',
+            objectSubNest: {
+                objSubKey1: 'objSubKey1 data',
+                objSubKey2: 'objSubKey2 data',
+            },
+        },
+    ],
+    nest2: [
+        {
+            deeperSubNest1: 'subNest1 data from the deep',
+            deeperSubNest2: 'subNest2 data from the deep',
+            deepObjSubNest: {
+                deepObjSubKey1: 'objSubKey1 data from the deep',
+                nestedObjectInNestObj: {
+                    theGreatDeepNesting: 'Data from the deepest depths of all!',
+                },
+                deepObjSubKey2: 'objSubKey2 data from the deep',
+            },
+        },
+    ],
+}
+
+function cycleArray(param) {
+    if (Array.isArray(param)) {
+        for (const element of param) {
+            cycleArray(element)
+        }
+    } else if (typeof param === 'object') {
+        for (const key in param) {
+            console.log(key)
+            cycleArray(param[key])
+        }
+    } else {
+        console.log(param)
+    }
+}
+
+cycleArray(nestedObject)
+
+//TODO: FURTHER FUNCTION REVIEW!!
+
+//Define: Function - the most important unit of code in JavaScript.
+// they serve as a way to group together related bits of JS code.
+
+//Define: Abstraction - latin roots of "to pull away."
+// it's the impression or the 'take away' that we get from a set of data.
+// ex: PB&J - takes bread, peanut butter, jelly, a knife, a plate, a napkin, etc...
+
+// to "execute" or "call" the function you add () after its name
+
+// Define: Generalization by using Paramaters and Arguments
+// for ex if we made a function called exerciseDog(dogName, dogBreed)
+// this function would be generalized, or abstracted through the paramaters.
+
+//so if we logged the parameter names with interpolation, this is an idea of generalization
+
+// in conclusion
+
+function functionName(parameter1, parameter2, parameter3) {
+    //body code goes here
+}
--- objectIteration.js
+++ objectIteration.js
@@ -113,3 +113,331 @@ for (const key in address) {
 // ARRAYS ARE OBJECTS
 // but don't use FOR...IN in arrays a a general rule
 // we have FOR...OF now
+
+//TODO: OBJECTS IN OBJECTs
+
+const userInfo = {
+    firstName: 'Avi',
+    lastName: 'Flombaum',
+    company: {
+        name: 'Flatbook Labs',
+        jobTitle: 'Developer Apprentice',
+    },
+    friends: [
+        /* notice how this is an OBJECT inside an ARRAY */
+        {
+            firstName: 'Nancy',
+            lastName: 'Burgess',
+            company: {
+                /* This is an object inside an object that is inside an array */
+                name: 'Flatbook Labs',
+                jobTitle: 'Developer Apprentice',
+            },
+        },
+        /* THIS IS WHERE A STRONG GRASP ON THE SCOPE OF YOUR CODE BECOMES SO IMPORTANT */
+        {
+            firstName: 'Corinna',
+            lastName: 'Jackson',
+            company: {
+                name: 'Flatbook Labs',
+                jobTitle: 'Lead Developer',
+            },
+        },
+    ],
+    projects: [
+        {
+            title: 'Flatbook',
+            description:
+                'The premier Flatiron School-based social network in the world.',
+        },
+        {
+            title: 'Scuber',
+            description:
+                'A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.',
+        },
+    ],
+}
+
+// FIXME: we are going to practice accessing some of these objects within and their elements/properties.
+
+// for a property at the top level of our object we can grab a value using dot.notation
+
+userInfo.lastName
+//=> Flombaum
+
+// if the property we're accessing is nested inside another object, we just append the additional keys to
+
+userInfo.company.jobTitle
+//=> Developer Apprentice
+
+// if the property is nested inside an arry, we need to specify the index in the array for the object that we want.
+
+userInfo.friends[0].firstName
+//=> Nancy
+
+userInfo.projects[1].title
+//=> Scuber
+
+// FIXME: You can work with nested ARRAYS WITHIN ARRAYS WOOOOOT
+// it's really not much different than working with objects
+
+const letters = ['a', ['b', ['c', ['d', ['e']], 'f']]]
+
+// how would you access the first letter 'e'?
+
+letters[1] // we are grabbing the second element of the entire array here
+
+letters[1][1] // then we are grabbing the second element of that element
+
+letters[1][1][1] // the second element of the previous element
+
+letters[1][1][1][1] // see the pattern here
+
+letters[1][1][1][1][0] // finally we want the first element in that final nested arrays
+
+// remember that each lookup (each set of square brackets) "drills down" into each successive nested array.
+
+//TODO: Iterating over nested objects and arrays
+
+// in this next example we are creating a for...in loop within a function
+// notice further below we are targeting userInfo as the "const key" from our userInfo object above.
+// during the code block where the console.log happens we are using [] notation to target the KEYS of the object and log them.
+// the target being the "target" of the object
+
+function shallowIterator(target) {
+    for (const key in target) {
+        console.log(target[key])
+    }
+}
+
+shallowIterator(userInfo)
+// LOG: Avi
+// LOG: Flombaum
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Nancy
+// LOG: Burgess
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Corinna
+// LOG: Jackson
+// LOG: Flatbook Labs
+// LOG: Senior Developer
+// LOG: Flatbook
+// LOG: The premier Flatiron School-based social network in the world.
+// LOG: Scuber
+// LOG: A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.
+
+//FIXME:
+// we can also use it in arrays!!! the for...in loop that is
+
+const primes = [2, 3, 5, 7, 11]
+
+shallowIterator(primes)
+// LOG: 2
+// LOG: 3
+// LOG: 5
+// LOG: 7
+// LOG: 11
+
+// this is where we can learn why FOR...IN isn't the best iterator to use with arrays. (cross-browser consisistency is an issue)
+
+//this function just can't handle NESTED COLLECTIONS
+// it has no concept of DEPTH
+const numbers = [1, [2, [4, [5, [6]], 3]]]
+
+shallowIterator(numbers)
+// LOG: 1
+// LOG: [2, [4, [5, [6]], 3]]
+
+// when it tries to iterate over the above nested NUMBERS array, it only sees 2 elements and calls it a day
+
+// by using an IF condition and the TYPEOF operator we can tackle nested arrays
+
+function advancedIterator(target) {
+    for (const key in target) {
+        if (typeof target[key] === 'object') {
+            for (const nestedKey in target[key]) {
+                console.log(target[key][nestedKey])
+            }
+        } else {
+            console.log(target[key])
+        }
+    }
+}
+
+advancedIterator(numbers)
+// LOG: 1
+// LOG: 2
+// LOG: [4, [5, [6]], 3]
+
+// we can see that we've gone two level deep now.
+// from here we will have to add a new FOR...IN statement for every level of nesting. This can quickly baloon our function to an unmanageable size
+// because of this we would also have to know exactly the target structure ahead of time and update our function accordingly.
+
+//TODO: THIS IS WHERE WE TALK ABOUT RECURSION
+//      RECURSION
+// Essentially it is function that calls itself
+
+// Rewriting our advancedIterator() to take advantage of recursion
+
+function deepIterator(target) {
+    if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+    } else {
+        console.log(target)
+    }
+}
+
+// ok take that same function and let's add some console.logs for visualization
+
+function deepIterator(target) {
+    console.log('Argument: ', target)
+    if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+    } else {
+        console.log('Logged value: ', target)
+    }
+}
+
+const nestedArray = [1, [2, [4, [5, [6]], 3]]]
+
+deepIterator(nestedArray)
+
+/*
+Argument:  [ 1, [ 2, [ 4, [Array], 3 ] ] ]   //.. as the function runs we get our first argument logged. consisting of the entire array
+Argument:  1                                 //.. if typeof target is an object. we are console logging the argument of the array again
+Logged value:  1                             //.. this is because the KEY in TARGET object that we have "argued" is now gonna re-run the function.. this time targeting the KEY of the TARGET
+Argument:  [ 2, [ 4, [ 5, [Array] ], 3 ] ]   //.. else we log LOGGED VALUE: TARGET above
+Argument:  2
+Logged value:  2                             //.. the cycle has started over... move down into the nest of arrays.. RECURSION WOOO
+Argument:  [ 4, [ 5, [ 6 ] ], 3 ]
+Argument:  4                                 //.. JavaScript is continuing reading on, moving down the line.. after every else occurs diving further down
+Logged value:  4
+Argument:  [ 5, [ 6 ] ]                      
+Argument:  5
+Logged value:  5
+Argument:  [ 6 ]
+Argument:  6
+Logged value:  6
+Argument:  3
+Logged value:  3                             //.. Finally logging the final property of the final ordered array
+*/
+
+//TODO: WE Can see how this is used on our userInfo object as well
+
+deepIterator(userInfo)
+// LOG: Avi
+// LOG: Flombaum
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Nancy
+// LOG: Burgess
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Corinna
+// LOG: Jackson
+// LOG: Flatbook Labs
+// LOG: Lead Developer
+// LOG: Flatbook
+// LOG: The premier Flatiron School-based social network in the world.
+// LOG: Scuber
+// LOG: A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.
+
+// you can see how you could add a COUNTER to keep track of how many times our function recursively invoked itself.
+
+let counter = 0
+
+function deepIterator(target) {
+    counter++
+
+    if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+    } else {
+        console.log(target)
+    }
+}
+
+//TODO: Array Static Method        Array.isArray()
+//      Used to determine whether a variable is an array
+
+function deepIterator(target) {
+    if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+
+        /* ARRAY.ISARRAY IN ACTION RIGHT HERE */
+    } else if (Array.isArray(target)) {
+        console.log('We found an array')
+        // iterate through the array
+    } else {
+        // console.log(target);
+    }
+}
+
+deepIterator(userInfo)
+
+// notice if you run the above code with Array.isArray() method we get nothing
+// we need to reverse the order of our conditions.
+
+function deepIterator(target) {
+    if (Array.isArray(target)) {
+        // iterate through the array
+        console.log('We found an array')
+    } else if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+    } else {
+        console.log(target)
+    }
+}
+
+deepIterator(userInfo)
+// LOG: Avi
+// LOG: Flombaum
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: We found an array
+// LOG: We found an array
+
+//TODO: Now imagine using the FOR...OF within our IF statement
+
+function deepIterator(target) {
+    if (Array.isArray(target)) {
+        for (const element of target) {
+            deepIterator(element)
+        }
+    } else if (typeof target === 'object') {
+        for (const key in target) {
+            deepIterator(target[key])
+        }
+    } else {
+        console.log(target)
+    }
+}
+
+deepIterator(userInfo)
+// LOG: Avi
+// LOG: Flombaum
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Nancy
+// LOG: Burgess
+// LOG: Flatbook Labs
+// LOG: Developer Apprentice
+// LOG: Corinna
+// LOG: Jackson
+// LOG: Flatbook Labs
+// LOG: Lead Developer
+// LOG: Flatbook
+// LOG: The premier Flatiron School-based social network in the world.
+// LOG: Scuber
+// LOG: A burgeoning startup helping busy parents transport their children to and from all of their activities on scooters.
